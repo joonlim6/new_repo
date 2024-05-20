a8590c3bd816d204f227e92d8079b9474dec2e65
joonlim6 <limjoon629@gmail.com>
2024-05-20 22:49:57 +0900

    init commit

---
+++ helloworld.py
@@ -0,0 +1,15 @@
+print('hello world')
+a = "python is amazing"
+
+print("%s는 %d살 입니다" %('준석이',20))
+print("{}는 {}살 입니다".format('준석이',20))
+
+age = 20
+name = "준석이"
+
+print(f"{name}는 {age}살 입니다")
+
+boxes = {3,2,1,3,3}
+candles = {4,5,3}
+
+print(boxes | candles)
